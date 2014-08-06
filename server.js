13ba1b6c34fb99784e5c64af715525e3394eda83
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-08-06 09:51:57 -0700

    version 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Hello, my friends\n");
+	response.end("Hello, version 3\n");
 });
 
 server.listen(80);
