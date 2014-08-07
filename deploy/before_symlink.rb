4265c3e8a8ac0abccb565d8728c85d199c4d10a7
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-08-07 13:19:06 -0700

    test3

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -3,5 +3,6 @@ Chef::Log.info("Custom deployment")
 
 execute "install bower components" do
 	user 
-	command "npm install bower && node_modules/.bin/bower install"
+	command "npm install bower"
+	command "node_modules/.bin/bower install"
 end
